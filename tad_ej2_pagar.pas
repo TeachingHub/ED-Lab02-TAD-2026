fb841b287f46e17c05d25ebc1e5d6e959be8e492
Sergio Cavero <sergio.cavero@urjc.es>
2026-02-12 12:21:16 +0100

    FIX: bug check payment out of the case

--- tad_ej2_pagar.pas
+++ tad_ej2_pagar.pas
@@ -1,66 +1,26 @@
-{ tad_ej2_pagar.pas - Programa Principal }
-program tad_ej2_pagar;
+unit uPagoBizum;
 
-uses
-   uPagoTarjeta, uPagoTransferencia, uPagoBizum;
+interface
 
-var
-  cantidad: real;
-  pagoValido: boolean;
-  opcion: integer;
-  data : string;
+procedure IniciarPago;
+procedure RealizarPago(numeroTelefono: string; monto: real);
+function ValidarPago(numeroTelefono: string): boolean;
 
+implementation
+
+procedure IniciarPago;
 begin
-    
-    // Leer monto
-    write('Introduce la cantidad a pagar: ');
-    readln(cantidad);
-    
-    // Seleccionar método de pago
-    writeln('Selecciona el método de pago:');
+  write('Introduce el número de teléfono para Bizum: ');
+end;
 
-    writeln('1. Tarjeta de crédito');
-    writeln('2. Transferencia bancaria');
-    writeln('3. Bizum');
-    write('Opción: ');
-    readln(opcion);
+procedure RealizarPago(numeroTelefono: string; monto: real);
+begin
+  writeln('Realizando pago por Bizum de ', monto:0:2, ' euros al número ', numeroTelefono);
+end;
 
-    // Realizar pago
-    case opcion of
-        1: 
-        begin
-          uPagoTarjeta.IniciarPago;
-          readln(data);
-          uPagoTarjeta.RealizarPago(data, cantidad);
-          pagoValido := uPagoTarjeta.ValidarPago(data);
-        end;
-        2:
-        begin
-          uPagoTransferencia.IniciarPago;
-          readln(data);
-          uPagoTransferencia.RealizarPago(data, cantidad);
-          pagoValido := uPagoTransferencia.ValidarPago(data);
-        end;
-        3:
-        begin
-          uPagoBizum.IniciarPago;
-          readln(data);
-          uPagoBizum.RealizarPago(data, cantidad);
-          pagoValido := uPagoBizum.ValidarPago(data);
-        end;
+function ValidarPago(numeroTelefono: string): boolean;
+begin
+  ValidarPago := (length(numeroTelefono) = 9);
+end;
 
-        else
-        begin
-          writeln('Opción no válida');
-        end;
-
-        if pagoValido then
-        begin
-          writeln('Pago realizado correctamente');
-        end
-        else
-        begin
-          writeln('Pago no válido');
-        end;
-    end;
 end.
